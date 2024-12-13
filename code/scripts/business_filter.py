7bde9f5fd68e8c2ba9ef84ccdbf16c9e67348d84
Huiwen Lian <lianhuiwen@utexas.edu>
2024-12-13 14:48:49 -0600

    little change

--- code/scripts/business_filter.py
+++ code/scripts/business_filter.py
@@ -1,17 +1,19 @@
 import json
 from database import client
-# 输入文件路径
+
 input_file_path = "data/yelp_academic_dataset_business.json"
-# 要保留的列名
+'''The file path for input data'''
 columns = [
-    "business_id", "name", "state", "city", "postal_code",
+
+    "business_id", "name", "state", "city", "address","postal_code",
     "latitude", "longitude", "stars", "review_count"
 ]
-# 要插入的表名
+
+
 table_name = "business"
-# 过滤后的数据存储列表
+
 filtered_data = []
-# 读取JSON文件并过滤数据
+'''A list to store filtered data'''
 with open(input_file_path, "r", encoding="utf-8") as file:
     for line in file:
         business = json.loads(line.strip())
@@ -22,17 +24,16 @@ with open(input_file_path, "r", encoding="utf-8") as file:
         ):
             filtered_business = {key: business.get(key, None) for key in columns}
             filtered_data.append(filtered_business)
-# 连接到数据库并插入数据
+
 try:
-    # 建立数据库连接
     
-    # 创建表（如果不存在）
     create_table_query = f"""
     CREATE TABLE IF NOT EXISTS {table_name} (
         business_id VARCHAR PRIMARY KEY,
         name VARCHAR,
         state VARCHAR,
         city VARCHAR,
+        address VARCHAR,
         postal_code VARCHAR,
         latitude FLOAT,
         longitude FLOAT,
@@ -42,7 +43,7 @@ try:
     """
     client.cursor().execute(create_table_query)
     client.commit()
-    # 插入数据
+    
     insert_query = f"""
     INSERT INTO {table_name} ({", ".join(columns)})
     VALUES ({", ".join(["%s"] * len(columns))})
@@ -51,7 +52,7 @@ try:
     for business in filtered_data:
         client.cursor().execute(insert_query, tuple(business.values()))
     
-    # 提交更改
+    '''commit changes'''
     client.commit()
     print(f"成功插入 {len(filtered_data)} 条数据到表 {table_name}")
 except Exception as e:
--- code/scripts/database.py
+++ code/scripts/database.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-# 数据库连接配置
+'''Connection database configuration'''
 db_config = {
     "dbname": os.getenv("DB_NAME"),
     "user": os.getenv("DB_USER"),
